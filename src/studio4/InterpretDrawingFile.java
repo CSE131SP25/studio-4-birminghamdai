2610fff7fb3b02190e858a607e0804116b68906b
bella <bella@localhost>
2025-02-19 16:55:02 -0600

    Finished studio

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,48 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String type = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double majorAxis = in.nextDouble();
+		double minorAxis = in.nextDouble();
+		
+		StdDraw.setPenColor (r,g,b);
+		
+		
+		if (type.equals("ellipse")) {
+			if (filled == true) {
+				StdDraw.filledEllipse(x,y,majorAxis,minorAxis);
+			}else {
+				StdDraw.ellipse(x,y,majorAxis,minorAxis);
+			}
+		}
+		
+		if (type.equals("rectangle")) {
+			if (filled == true) {
+				StdDraw.filledRectangle(x, y, majorAxis, minorAxis);
+			}else {
+				StdDraw.rectangle(x, y, majorAxis, minorAxis);
+			}
+		}
+		
+		if (type.equals("triangle")) {
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			double [] triangleX = {x,majorAxis,halfWidth};
+			double [] triangleY = {y,minorAxis,halfHeight};
+			if (filled == true) {
+				StdDraw.filledPolygon(triangleX,triangleY);
+			}else {
+				StdDraw.polygon(triangleX,triangleY);
+			}
+		}
+		
+		StdDraw.show();
 		
 	}
 }
